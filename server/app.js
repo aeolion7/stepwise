b88ac5299268e7307e854b87e8e7f2ab5f2679f7
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-01 13:50:18 -0500

    error handling endware

--- server/app.js
+++ server/app.js
@@ -11,6 +11,13 @@ app.get('/', (req, res, next) => {
   res.status(200).send('index.html');
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res
+    .status(err.status || 500)
+    .send(err.message || '500: Internal Server Error.');
+});
+
 app.listen(PORT, () => {
-  console.log('Application is live on port ', PORT);
+  console.log('Application is live on port', PORT);
 });
